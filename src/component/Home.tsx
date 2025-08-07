52c3708ad2d23e112904e2fb418e05a5089ec89a
Qofy <148817944+Qofy@users.noreply.github.com>
2025-08-07 11:53:36 +0200

    worked on the home page header

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,17 +1,22 @@
 import { Truck, Shield, Star } from "lucide-react";
 import "../style/home.css";
+// import Product from "../assets/modern.jpg"
+
 function Header() {
   return (
    <div className="main-container">
-    <section>
+    <div className="header-container">
+<section className="main-section">
       <h1 className="main-title">
         Smart Gadgets for <span>Mordern Homes</span>
       </h1>
-      <p className="">
+      <p className="main-description">
         Discover cutting-edge home gadgets that make your life easier, smarter, and more connected. From smart speakers to wireless chargers, we have everything you need.
       </p>
-      <button className="main-btn btn">Shop Now &#8594;</button>
-      <button className="main-btb btn">View Catalog</button>
+      <div className="btn">
+      <button className="main-btn1 btns">Shop Now &#8594;</button>
+      <button className="main-btn btns">View Catalog</button>
+      </div>
       <div className="features">
         <div className="feature-item">
           <Star/>
@@ -19,14 +24,23 @@ function Header() {
         </div>
         <div className="feature-item">
           <Shield/>
-          <p>"-Years Warranty</p>
+          <p>Years Warranty</p>
         </div>
         <div className="feature-item">
           <Truck/>
-          <p>Free Shipping on Orders Over $50</p>
+          <p>Free Shipping </p>
         </div>
       </div>
+      
     </section>
+    {/* <div className="product-icon">
+        <span>50+</span>
+        <p>product</p>
+      </div> */}
+      <div className="product-image">
+      </div>
+    </div>
+    
 
    </div>
   );
